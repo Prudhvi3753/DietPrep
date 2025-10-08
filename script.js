c71c028a8fa7cfecb347955c35e70b68604f92b8
Prudhvi3753 <prudhvich45@gmail.com>
2025-10-08 10:18:08 +0530

    Apex Project

---
+++ script.js
@@ -0,0 +1,4 @@
+const getStartedBtn = document.getElementById('getStartedBtn');
+getStartedBtn.addEventListener('click', function() {
+    alert("Welcome! Let's create your personalized meal plan.");
+});
